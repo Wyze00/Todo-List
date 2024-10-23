27962d5b0b234df8ab15b187fad6494d277aa9b0
Wyze00 <144517602+Wyze00@users.noreply.github.com>
2024-10-23 20:02:00 +0700

    Add files via upload

---
+++ Todo-(DOM)/script.js
@@ -0,0 +1,79 @@
+const button = document.getElementById('input-button');
+const awal = document.querySelector('.tugas-awal');
+const akhir = document.querySelector('.tugas-akhir');
+
+button.addEventListener('click',() => {
+
+    const text = document.getElementById('input-text').value;
+    const date = document.getElementById('input-date').value.replace(/[-]/g,'/');
+
+    document.getElementById('input-text').value = '';
+    document.getElementById('input-date').value = '';
+
+    tambahAwal(text,date);
+})
+
+awal.addEventListener('click',function(e){
+
+    if(e.target.alt == 'check'){
+
+        const text = e.target.parentNode.parentNode.firstChild.children[0].innerText;
+        const date = e.target.parentNode.parentNode.firstChild.children[1].innerText;
+
+        tambahAkhir(text,date);
+
+        awal.removeChild(e.target.parentNode.parentNode);
+    }
+})
+
+akhir.addEventListener('click',function(e){
+
+    if(e.target.alt == 'trash'){
+
+        akhir.removeChild(e.target.parentNode.parentNode);
+    }
+
+    if(e.target.alt == 'undo'){
+
+        const text = e.target.parentNode.parentNode.firstChild.children[0].innerText;
+        const date = e.target.parentNode.parentNode.firstChild.children[1].innerText;
+
+        tambahAwal(text,date);
+
+        akhir.removeChild(e.target.parentNode.parentNode);
+    }   
+})
+
+function tambahAwal(text,date){
+    const div = document.createElement('div');
+
+    div.classList.add('time');
+    div.innerHTML = `<div class="head">
+                        <p>${text}</p>
+                        <small>${date}</small>
+                    </div>
+
+                    <div class="image">
+                        <img src="./src/check.svg" alt="check" width="40px">
+                    </div>`
+    
+    awal.appendChild(div);
+}
+
+function tambahAkhir(text,date){
+
+    const div = document.createElement('div');
+    div.classList.add('time');
+
+    div.innerHTML = `<div class="head">
+                        <p>${text}</p>
+                        <small>${date}</small>
+                     </div>
+
+                    <div class="image">
+                        <img src="./src/trash.svg" alt="trash" width="40px">
+                        <img src="./src/undo.svg" alt="undo" width="40px">
+                     </div>`
+
+    akhir.append(div);
+}
